6414cfb9ecde521925a9725f49d0939c4a8df93b
Matthew Kindt <matthewkindt96@yahoo.com>
2020-12-03 21:14:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,10 +13,11 @@ def list_dwarves(array)
   hash = Hash.new
   %w(array).each.with_index(1) { |array, index|
     hash[array] = index
-    puts "#{index}. #{"Dopey"}",
-    "#{index}. #{"Grumpy"}",
-    "#{index}. #{"Bashful"}"
+    puts "#{index}. #{"Dopey"}"
+    puts "#{index}. #{"Grumpy"}"
+    puts "#{index}. #{"Bashful"}"
   }
+  hash
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
