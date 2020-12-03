0b1a3929700beb50a3122ba5791d367305767f2d
Matthew Kindt <matthewkindt96@yahoo.com>
2020-12-03 20:33:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def greet_characters(array)
   array.each do |array|
-   puts "#{"Hello".capitalize} Dopey!/"
-   puts "#{"Hello".capitalize} Grumpy!/"
-   puts "#{"Hello".capitalize} Bashful!/"
+   puts "#{"Hello".capitalize} "Dopey!""
+   puts "#{"Hello".capitalize} "Grumpy!""
+   puts "#{"Hello".capitalize} "Bashful!""
 end 
   # Use `each` to enumerate over the provided array
   #
