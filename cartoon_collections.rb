294a40a186bfaac359c3d415a225d04d3c6cba78
Matthew Kindt <matthewkindt96@yahoo.com>
2020-12-03 20:40:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
+  array.each_with_index
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
